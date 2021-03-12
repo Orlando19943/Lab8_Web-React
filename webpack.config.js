128cd34ddfdc9cd8882d3d360d5f8e2e2df6e6ea
Orlando Cabrera <orlando.osberto@gmail.com>
2021-03-12 13:07:38 -0600

    Instalando bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,10 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
+      { test: /\.css$/i, use: ["style-loader", "css-loader"]},
+      { test: /\.s[ac]ss$/i, use: ["style-loader", "css-loader", "sass-loader"]},
+      { test: /\.(png|jpe?g|gif)$/i, loader: 'file-loader', options: {name: '[path][name].[ext]' }, },
+      { test: /\.js$/, loader: "babel-loader", exclude: /(node_modules)/, },
     ]
   },
   plugins: [
