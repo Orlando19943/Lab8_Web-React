3d15ad28edc779db4c95b53276c434d4573fe1b4
Orlando <orlando.osberto@gmail.com>
2021-03-25 12:30:14 -0600

    Cambiando la configuracion de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,30 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, '/dist'),
-    filename: '[name].js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    library: 'test',
+    libraryTarget: 'window',
+    libraryExport: 'default'
   },
+  plugins: [
+    new MiniCssExtractPlugin(), new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      chunks: ["index"]
+    }
+    ),
+  ],
   module: {
     rules: [
       { test: /\.css$/i, use: ["style-loader", "css-loader"]},
-      { test: /\.s[ac]ss$/i, use: ["style-loader", "css-loader", "sass-loader"]},
-      { test: /\.(png|jpe?g|gif)$/i, loader: 'file-loader', options: {name: '[path][name].[ext]' }, },
-      { test: /\.js$/, loader: "babel-loader", exclude: /(node_modules)/, },
-      {test: /\.(svg|eot|woff|woff2|ttf)$/,use: ['file-loader']}
+      { test: /\.s[ac]ss$/i, use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]},
+      { test: /\.(png|jpe?g|gif|svg|eot|woff|woff2|ttf)$/i, loader: 'file-loader', options: {name: '[path][name].[ext]' }, },
+      { test: /\.js$/, loader: "babel-loader", exclude: /(node_modules)/, }
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
+  }
 };
